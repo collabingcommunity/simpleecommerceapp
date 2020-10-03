834f0124123a86c252b09d937c0baab185ffca90
collabingcommunity <72201421+collabingcommunity@users.noreply.github.com>
2020-10-02 21:42:56 -0300

    Criação de projeto de reactjs e redux (#1)
    Co-authored-by: robertomorel <game.developer.br@gmail.com>

--- projredux/src/App.tsx
+++ projredux/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import Home from './pages/Home';
+import store from './store';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Provider store={store}>
+      <Home />
+    </Provider>  
   );
 }
 
